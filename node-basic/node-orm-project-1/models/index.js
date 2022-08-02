a51cd924bcacbb4272a805d6a7a4c9457313ac7e
YUZAMIN <untmd.crmn@gmail.com>
2022-08-03 02:15:33 +0900

    Node.js 기초 - 모델 관리 index.js 모듈 구성 및 프로젝트에 MySQL 연결 설정

--- node-basic/node-orm-project-1/models/index.js
+++ node-basic/node-orm-project-1/models/index.js
@@ -1,37 +1,40 @@
-'use strict';
+const path = require("path");
+const Sequelize = require("sequelize");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+/*
+  개발 모드 환경 설정
+    - 환경 변수
+    - 실배포 시 'production'으로 변경
+*/
+const env = process.env.NODE_ENV || "development";
+
+// db 연결 환경 설정 정보
+const config = require(path.join(__dirname, "..", "config", "config.json"))[
+  env
+];
+
+// db 객체 생성
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// db 연결 정보로 시퀄라이즈 ORM 객체 생성
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
+/*
+  db 처리 객체에 시퀄라이즈 정보 맵핑 처리
+    - 이후 db 객체를 통해 데이터 관리 가능
+    - sequelize: db 연결 정보를 포함한 db 제어 객체 속성 (CRUD)
+    - Sequelize: Sequelize 패키지에서 제공하는 각종 데이터 타입 및 관련 객체 정보
+*/
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+// 회원 모델 모듈 파일 참조 및 db 속성 정의
+// db.Member = require("./member.js")(sequelize, Sequelize);
+
+// db 객체 외부로 노출
 module.exports = db;
